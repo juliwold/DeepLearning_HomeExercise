fc97859bce007091e1460042e6e986dc91eb0f81
juliwold <julius.wold@nmbu.no>
2023-06-05 14:52:07 +0200

    Train yolo models
    Grid search:
    
    - Model size: s, m, x.
    - Imgsz: 256, 640, 1024.

--- scripts/4-model_training.py
+++ scripts/4-model_training.py
@@ -1,73 +1,87 @@
-import os
-import yaml
-
-import pandas as pd
-from itertools import product
-import comet_ml
-
-comet_ml.init(project_name='seedling_detection_YOLOv8')
-
-import ultralytics
-
-ultralytics.checks()
-
-path_to_annotated = "..\\data\\annotated_data\\train"
-annotations = ("my_annotations", "all_annotations")
-
-data_configs = []
-for a in annotations:
-    path_to_tiles = f"{path_to_annotated}\\{a}"
-    config_data = {
-        'path': os.path.abspath(path_to_tiles),
-        'train': os.path.abspath(f"{path_to_tiles}\\train\\images"),
-        'val': os.path.abspath(f"{path_to_tiles}\\val\\images"),
-        'test':os.path.abspath("..\\data\\annotated_data\\test\\images"), # this is by default
-        'names': {
-            0: 'tree'
+if __name__ == '__main__':
+    import os
+    import yaml
+    
+    import pandas as pd
+    from itertools import product
+    import comet_ml
+    
+    experiment = comet_ml.init()
+    
+    # experiment = comet_ml.Experiment(
+        # api_key="API-key",
+        # project_name=""
+    # )
+    
+    import ultralytics
+    import torch
+        
+    torch.cuda.empty_cache()
+    torch.cuda.memory_allocated()
+    torch.cuda.memory_reserved()
+    ultralytics.checks()
+    
+    
+    path_to_annotated = "..\\data\\annotated_data\\train"
+    annotations = ("my_annotations", "all_annotations")
+    
+    data_configs = []
+    for a in annotations:
+        path_to_tiles = f"{path_to_annotated}\\{a}"
+        config_data = {
+            'path': os.path.abspath(path_to_tiles),
+            'train': os.path.abspath(f"{path_to_tiles}\\train\\images"),
+            'val': os.path.abspath(f"{path_to_tiles}\\val\\images"),
+            'test':os.path.abspath("..\\data\\annotated_data\\test\\images"), # this is by default
+            'names': {
+                0: 'tree'
+            }
         }
+        config = f"{path_to_tiles}\\train_config.yaml"
+        data_configs.append(config)
+        with open(config, "w") as file:
+            yaml.dump(config_data, file)
+    
+    hyperamaters = {
+        "data": data_configs,
+        "model": ["yolov8n.pt", "yolov8m.pt", "yolov8x.pt"],
+        "img_size": [256, 640, 1024],
     }
-    config = f"{path_to_tiles}\\train_config.yaml"
-    data_configs.append(config)
-    with open(config, "w") as file:
-        yaml.dump(config_data, file)
-
-hyperamaters = {
-    "data": data_configs,
-    "model": ["yolov8n.pt", "yolov8s.pt", "yolov8m.pt", "yolov8l.pt", "yolov8x.pt"],
-    "img_size": [250, 500, 1000, 2000],
-}
-model_settings = pd.DataFrame(
-    [row for row in product(*hyperamaters.values())],
-    columns=hyperamaters.keys()
-)
-
-model_settings["annotations"] = model_settings["data"].str.extract("(my_annotations|all_annotations)")
-model_settings["project"] =  "models\\" + model_settings["annotations"]
-model_settings["name"] = (
-    model_settings["annotations"] + "_" +
-    model_settings["model"] + "_" +
-    model_settings["img_size"].astype(str)
-)
-
-# Test run
-hyperamater_settings = model_settings.iloc[0, :]
-
-model = ultralytics.YOLO(hyperamater_settings["model"])
-
-model.train(
-    data = hyperamater_settings["data"],
-    epochs = 3,
-    imgsz = int(hyperamater_settings["img_size"]),
-    name = "test_run",
-    project = ".test_runs"
-)
-
-for i, settings in model_settings.iterrows():
-    model = ultralytics.YOLO(settings["model"])
-    model.train(
-        data = settings["data"],
-        epochs = 350,
-        imgsz = int(settings["img_size"]),
-        name = settings["name"],
-        project = settings["project"]
+    model_settings = pd.DataFrame(
+        [row for row in product(*hyperamaters.values())],
+        columns=hyperamaters.keys()
     )
+    
+    model_settings["annotations"] = model_settings["data"].str.extract("(my_annotations|all_annotations)")
+    model_settings["project"] =  "..\\models\\" + model_settings["annotations"]
+    model_settings["name"] = (
+        model_settings["annotations"] + "_" +
+        model_settings["model"] + "_" +
+        model_settings["img_size"].astype(str)
+    )
+
+    # Test run
+    # hyperamater_settings = model_settings.iloc[0, :]
+    
+    # model = ultralytics.YOLO(hyperamater_settings["model"])
+    
+    # model.train(
+        # data = hyperamater_settings["data"],
+        # epochs = 3,
+        # imgsz = int(hyperamater_settings["img_size"]),
+        # name = "test_run",
+        # project = ".test_runs"
+    # )
+    
+    start = 6
+    for i, settings in model_settings.iloc[start:, :].iterrows():
+        model = ultralytics.YOLO(settings["model"])
+        model.train(
+            data = settings["data"],
+            epochs = 350,
+            imgsz = int(settings["img_size"]),
+            name = settings["name"],
+            project = settings["project"],
+        )
+        del model
+        torch.cuda.empty_cache()
