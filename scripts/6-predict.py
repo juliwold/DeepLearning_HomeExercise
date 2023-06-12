a1feabe1fb255dd0c1f4d3fbe44d34e1ef01c53a
juliwold <julius.wold@nmbu.no>
2023-06-12 16:01:56 +0200

    Predict on tiles

---
+++ scripts/6-predict.py
@@ -0,0 +1,45 @@
+import os
+import glob
+
+from ultralytics import YOLO
+
+
+def path_to_model(model_dir, model_name):
+    model_path = os.path.join(model_dir, model_name, "weights", "best.pt")
+    return model_path
+
+
+def main():
+    #
+    path_to_tiles = os.path.join("..", "data", "tiles", "test_data")
+    tiles = glob.glob(os.path.join(path_to_tiles, "*"))
+    # Define models to predict with.
+    model_dir = os.path.join("..", "models")
+    models = [
+        {"name": "my_annotations_yolov8m.pt_640", "conf": 0.60},
+        {"name": "all_annotations__yolov8m.pt_640", "conf": 0.23},
+    ]
+
+    for model in models:
+        # Load model.
+        project = os.path.join(model_dir, model["name"])
+        try:
+            model_path = path_to_model(model_dir, model["name"])
+            yolo_model = YOLO(model_path)
+        except FileNotFoundError:
+            print("Model not found.")
+            continue
+        print(f"\n\nPredicting using {model['name']}.")
+
+        # Make predictions.
+        for tile in tiles:
+            print(f"Predicting for {os.path.basename(tile)}.")
+            yolo_model.predict(
+                source=tile,
+                project=project,
+                conf=model["conf"],
+                save=True,
+                save_txt=True,
+                save_conf=True,
+                line_width=1,
+            )
